2864c15384cc472241a1dc3e8a77fc5d19f4ae42
fuyuanwu <yfu@evolveconsulting.com.hk>
2016-07-29 18:56:37 +0800

    123

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,9 +1,10 @@
-'use strict';
+'use strict'
 
-import Base from './base.js';
-import fs from 'fs';
-import imagemagick from 'imagemagick-native';
-import crypto from 'crypto';
+import Base from './base.js'
+import fs from 'fs'
+import imagemagick from 'imagemagick-native'
+import crypto from 'crypto'
+import _ from 'lodash'
 
 export default class extends Base {
   /**
@@ -11,10 +12,14 @@ export default class extends Base {
    * @return {Promise} []
    */
   indexAction () {
-    const source = './img/8.jpg'
-    const dist = './img/after_resize.png'
+    let width = this.get('w') || 0
+    let height = this.get('h') || 0
+    let resize_style = this.get('rs') || aspectfill
+    let type = this.get('t') || 'png'
 
-    fs.stat('./img/8.jpg', function (err, stat) {
+    const source = './img/8.jpg'
+
+    fs.stat(source, function (err, stat) {
       if (err) return
       let size = stat.size
       let quality = 100
@@ -26,27 +31,38 @@ export default class extends Base {
       else if (size > 512) quality = quality * 0.9
 
       let srcData = fs.readFileSync(source)
-      let md5sum = crypto.createHash('md5');
+      let md5 = crypto.createHash('md5')
+      md5.update(srcData)
+      let md5Hex = md5.digest('hex')
+
+      const encode_filename_v1 = [ 'v1', md5Hex, type, width, height, resize_style ].join('&')
+      const deode_filename_v1 = [ v1, md5Hex, type, width, height, resize_style ] = encode_filename_v1.split('&')
+
+      const dist = `./img/${encode_filename_v1}`
 
       imagemagick.identify({
         srcData: srcData
       }, function (err, result) {
         if (err) return
-        console.log(result)
+
+        const options = {
+          srcData: srcData,
+          blur: 1,
+          // resizeStyle: resize_style, // aspectfill is the default, or 'aspectfit' or 'fill'
+          gravity: 'Center', // optional: position crop area when using 'aspectfill'
+          format: type,
+          quality: quality
+        }
+
+        if (width) options[ width ] = width
+        if (height) options[ height ] = height
+        if (resize_style) options[ resizeStyle ] = resize_style
+
+        fs.writeFileSync(dist, imagemagick.convert(options))
       })
 
-      fs.writeFileSync(dist, imagemagick.convert({
-        srcData: srcData,
-        blur: 1,
-        width: 500,
-        height: 500,
-        resizeStyle: 'aspectfill', // aspectfill is the default, or 'aspectfit' or 'fill'
-        gravity: 'Center', // optional: position crop area when using 'aspectfill'
-        format: 'PNG',
-        quality: quality
-      }))
     })
 
-    return this.display();
+    return this.display()
   }
 }
